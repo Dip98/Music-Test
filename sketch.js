f58f498ceb233bfbdfc9b8452e19088e75106164
Dipper98 <134005854+Dip98@users.noreply.github.com>
2024-06-13 16:05:23 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,9 @@
 var song;
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  song = loadSound('Survive-Soundtrack.mp3')l
 }
 function draw() {
+    song.play();
     rect(10, 10, 10, 10)
 }
